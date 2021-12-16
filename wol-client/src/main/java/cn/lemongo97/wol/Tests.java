3762220d3ebe79ccffcc153a5090f0cfbdc0c7b0
LemonGo97 <lemongo97@outlook.com>
2021-12-16 22:54:48 +0800

    修改正则表达式以适配Windows

--- wol-client/src/main/java/cn/lemongo97/wol/Tests.java
+++ wol-client/src/main/java/cn/lemongo97/wol/Tests.java
@@ -3,19 +3,20 @@ package cn.lemongo97.wol;
 import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.RuntimeUtil;
 
-import java.net.NetworkInterface;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Tests {
 
-    final static Pattern pattern = Pattern.compile(".*?((?:(?:2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(?:2[0-4]\\d|25[0-5]|[01]?\\d\\d?)).*?(\\w{2}(?::\\w{2}){5}).*");
+    final static Pattern IP_MAC_PATTERN = Pattern.compile(".*?((?:(?:2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(?:2[0-4]\\d|25[0-5]|[01]?\\d\\d?)).*?(\\w{2}(?:[:|-]\\w{2}){5}).*");
 
     public static void main(String[] args) {
         List<String> lines = RuntimeUtil.execForLines("arp -a");
         for (String line : lines) {
-            Matcher matcher = pattern.matcher(line);
+            Matcher matcher = IP_MAC_PATTERN.matcher(line);
             if (matcher.find()){
                 Console.log("---------------------------------");
                 Console.log("IP: ", matcher.group(1));
