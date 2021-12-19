b769eec8f6631d1967ed7d2738caecb74d5bcc17
LemonGo97 <lemongo97@outlook.com>
2021-12-19 20:15:16 +0800

    解决test报错

--- wol-client/src/test/java/cn/lemongo97/wolclient/ClientApplicationTests.java
+++ wol-client/src/test/java/cn/lemongo97/wolclient/ClientApplicationTests.java
@@ -1,9 +1,10 @@
 package cn.lemongo97.wolclient;
 
+import cn.lemongo97.wol.ClientApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ClientApplication.class)
 class ClientApplicationTests {
 
     @Test
