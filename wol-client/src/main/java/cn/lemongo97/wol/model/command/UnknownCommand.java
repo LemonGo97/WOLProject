b5fcc16f4c8c6204c065b5754203561c36398b79
LemonGo97 <lemongo97@outlook.com>
2021-12-19 23:04:37 +0800

    适配目前支持的三个命令

--- wol-client/src/main/java/cn/lemongo97/wol/model/command/UnknownCommand.java
+++ wol-client/src/main/java/cn/lemongo97/wol/model/command/UnknownCommand.java
@@ -1,8 +1,12 @@
 package cn.lemongo97.wol.model.command;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class UnknownCommand extends Command{
     @Override
     protected Object doCommand() {
-        return "success";
+        log.error("未知的命令，请检查客户端是否为最新版或上报社区 issue ！！！");
+        return "not support command !!!";
     }
 }
--- wol-client/src/main/java/cn/lemongo97/wol/tools/CommandFactory.java
+++ wol-client/src/main/java/cn/lemongo97/wol/tools/CommandFactory.java
@@ -1,9 +1,7 @@
 package cn.lemongo97.wol.tools;
 
 import cn.lemongo97.wol.Response;
-import cn.lemongo97.wol.model.command.Command;
-import cn.lemongo97.wol.model.command.ScanCommand;
-import cn.lemongo97.wol.model.command.UnknownCommand;
+import cn.lemongo97.wol.model.command.*;
 import cn.lemongo97.wol.utils.GsonUtil;
 import com.google.gson.reflect.TypeToken;
 
@@ -12,8 +10,11 @@ public class CommandFactory {
     public static Command getInstance(String json, String command) {
         switch (command) {
             case "scan":
-                Response<ScanCommand> response = GsonUtil.fromJson(json, new TypeToken<Response<ScanCommand>>() {});
-                return response.getBody();
+                return GsonUtil.fromJson(json, new TypeToken<Response<ScanCommand>>() {}).getBody();
+            case "wake":
+                return GsonUtil.fromJson(json, new TypeToken<Response<WakeSingleCommand>>() {}).getBody();
+            case "wake-batch":
+                return GsonUtil.fromJson(json, new TypeToken<Response<WakeBatchCommand>>() {}).getBody();
             default:
                 return new UnknownCommand();
         }
