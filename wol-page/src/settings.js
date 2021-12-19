d2d53e105fc50690f2f64e207ea26c023fe4011e
LemonGo97 <lemongo97@outlook.com>
2021-12-19 23:32:39 +0800

    提交前端页面

---
+++ wol-page/src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: 'Wake On Lan Cloud',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
