fdde7309193dd3077fb744afc30340be68dd07c3
LemonGo97 <lemongo97@outlook.com>
2021-12-25 15:04:08 +0800

    所有权限问题都已解决

---
+++ wol-server/src/main/java/cn/lemongo97/wol/controller/UserController.java
@@ -0,0 +1,30 @@
+package cn.lemongo97.wol.controller;
+
+import cn.hutool.core.map.MapUtil;
+import cn.lemongo97.wol.config.ResponseResult;
+import cn.lemongo97.wol.config.Result;
+import cn.lemongo97.wol.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author LemonGo97
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/info")
+    public Result getInfo() {
+        return Result.success(MapUtil.builder()
+                .put("name", "admin")
+                .put("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif").build());
+    }
+
+}
